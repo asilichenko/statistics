faaef9318291d0932fb2c9dfc28671a35ae558ae
Oleksii Sylichenko <a.silichenko@gmail.com>
2023-03-19 18:31:45 +0200

    refactoring

--- src/main/java/ua/in/asilichenko/statistics/NormalDistribution.java
+++ src/main/java/ua/in/asilichenko/statistics/NormalDistribution.java
@@ -27,32 +27,32 @@ public class NormalDistribution {
      */
     public static double probability(double mean, double stdDeviation, double bound) {
         final double z = calcZ(mean, stdDeviation, bound);
-        return (1 - erf(z)) / 2;
+        return (1 - erf(z / Math.sqrt(2))) / 2;
     }
 
     /**
      * @param mean         μ
      * @param stdDeviation σ
      * @param bound        x
-     * @return (μ - x) / (σ√2)
+     * @return (μ - x) / σ
      * @see <a href="https://en.wikipedia.org/wiki/Standard_score">Z-score</a>
      */
     public static double calcZ(double mean, double stdDeviation, double bound) {
-        return (mean - bound) / (stdDeviation * Math.sqrt(2));
+        return (mean - bound) / stdDeviation;
     }
 
     /**
      * Error function iterative calculated by Taylor series.
      * <a href="https://en.wikipedia.org/wiki/Error_function">Error function</a>
      *
-     * @param z (μ - bound) / (σ√2)
+     * @param x (μ - bound) / (σ√2)
      */
-    public static double erf(double z) {
+    public static double erf(double x) {
         double sum = 0;
         for (int n = 0; n < 11; n++) {
-            double sfsfsf = z / (2 * n + 1);
-            for (int i = 1; i <= n; i++) sfsfsf *= -z * z / i;
-            sum += sfsfsf;
+            double product = x / (2 * n + 1);
+            for (int k = 1; k <= n; k++) product *= -x * x / k;
+            sum += product;
         }
         return 2 / Math.sqrt(Math.PI) * sum;
     }
